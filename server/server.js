910086ad100c12559fda687c7e5985ccf9cb33b3
HONGYAN <zhy19930227@gmail.com>
2019-10-02 14:24:13 +1000

    chatting room finished main part

--- server/server.js
+++ server/server.js
@@ -25,10 +25,30 @@ io.sockets.on('connection', function(socket){
     console.log('User Connected: %s online', connections.length);     //display the connection leagth, how many users          
 
     socket.on('disconnect', function(data){
+        if(socket.username){
+            users.splice(users.indexOf(socket.username), 1);
+            updateUsers();
+        }
         connections.splice(connections.indexOf(socket), 1);
         console.log('User disconneted: %s online', connections.length);//display the disconnection leagth, how many users disconnection
     });
+
     socket.on('send message', function(data){
-        io.sockets.emit('new message', {msg:data});
+        io.sockets.emit('new message', {msg:data, user: socket.username});
     });
+
+    socket.on('new user', function(data, callback){
+        if(users.indexOf(data) != -1){
+            callback(false);
+        }else {
+            callback(true);
+            socket.username = data;
+            users.push(socket.username);
+            updateUsers();
+        }
+    });
+
+    function updateUsers(){
+        io.sockets.emit('get users', users);
+    }
 });
